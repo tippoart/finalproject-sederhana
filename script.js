a36f4a6a83411e31e8c3fc1ea7e1cd39567444a0
tippo <123719526+tippoart@users.noreply.github.com>
2023-04-09 16:20:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".multiple-text",{
+    strings:["FRONTEND DEVELOPER","FREELANCER","PHOTO DESIGN","FOOTBAL PLAYER","GAMERS"],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop:true
+});
